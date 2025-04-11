c58b1ceec3f5f96f5a0f4530036fc74eac0e79e1
Manuel Yang <3444220+mannyyang@users.noreply.github.com>
2025-04-11 01:30:40 -0600

    Created a test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-export interface Env {
-  AI: Ai;
-}
-
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-    const userMessage = await request.text();
-
-    const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-      prompt: userMessage,
-    });
-
-    return new Response(JSON.stringify(response));
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Test endpoint is working!");
   },
-} satisfies ExportedHandler<Env>;
+};
